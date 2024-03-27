e6b57fc34382f1f90b6ac74831f17f848c1bc592
Kaio1612 <128390525+Kaio1612@users.noreply.github.com>
2024-03-27 09:39:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,32 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+contadores[0].textContent = "Contagem regressiva";
+
+const tempoObjetivo1 = new Date("2024-10-05");
+
+contadores[0].textContent = tempoObjetivo1;
+
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+
